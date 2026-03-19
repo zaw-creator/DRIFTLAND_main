0359da6ef3cc116d144a534688927917b38428da
zaw-creator <74670055+zaw-creator@users.noreply.github.com>
2026-03-19 22:31:33 +0000

    fix the issues for the db not connected and admin portal login issue

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  turbopack: {
+    root: new URL('.', import.meta.url).pathname,
+  },
 };
 
 export default nextConfig;
